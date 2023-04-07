b76722ecb5903a3fd0e356db4ec16d25d5e433ec
Anna A <markova29@yandex.ru>
2023-04-07 21:16:53 +0300

    add Solution Java Syntax task 0219

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0219/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.pro.task02.task0219;
+
+/* 
+Баги робокопа
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int id = 1;
+        int age = 2;
+        String documents = "pasport";
+        String behavior = "good";
+    }
+}
