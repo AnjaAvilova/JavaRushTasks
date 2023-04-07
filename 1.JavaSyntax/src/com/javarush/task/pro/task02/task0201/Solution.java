6d39bf816396012708f6f637afda64da0c50fa6e
Anna A <markova29@yandex.ru>
2023-04-07 21:17:31 +0300

    add Solution Java Syntax task 0221

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0201/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task02.task0201;
+
+/* 
+«Я по объявлению…»
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int intValue = 10;
+        int size = 2;
+        int numericValue = 15;
+        int number = 1;
+
+    }
+}
