69597109a7effa8fd543f908be1277a3e30942c9
Anna A <markova29@yandex.ru>
2023-04-12 11:45:21 +0300

    add Solution Java Syntax task 0206

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0206/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task02.task0206;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        String word = "Hello";
+        String  phrase = "my";
+        String line = "friend";
+        String text = "fanny";
+
+
+    }
+}
