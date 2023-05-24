4772591b37136a05a31e7b8e2ad1fe6f1bea7070
Anna A <markova29@yandex.ru>
2023-05-24 12:49:30 +0300

    add Solution Java Syntax task 0622/3

--- 1.JavaSyntax/src/com/javarush/task/pro/task06/task0622/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0622/Solution.java
@@ -15,29 +15,58 @@ public class Solution {
     public static String winPhrase = "Амиго одержал победу над Диабло и выбил из его головы загадочный кристалл.";
     public static String loosePhrase = "Диабло победил Амиго.";
     public static int diabloPosition;
+    public static int amigoLives = 9;
+    public static int diabloLives = 9;
 
 
     public static void main(String[] args) {
         diabloPosition = getRandomNumber(4);
         findDiablo();
+        battle();
     }
 
     public static int getRandomNumber(int range) {
         return (int) (Math.random() * range) + 1;
     }
+
     public static void findDiablo() {
         Scanner scanner = new Scanner(System.in);
         System.out.println(getFirstPositionPhrase);
-        while(true) {
+        while (true) {
             int number = scanner.nextInt();
             if (number == diabloPosition) {
                 System.out.println(findDiabloPhrase);
                 break;
-            }
-            else {
+            } else {
                 System.out.println(getPositionPhrase);
             }
         }
+    }
+
+    public static void amigoLostLife() {
+        amigoLives = amigoLives - 1;
+    }
+    public static void diabloLostLife(){
+        diabloLives = diabloLives - 3;
+    }
+    public static int amigoAttacks() {
+       return getRandomNumber(3);
+
+    }
+
+    public static int diabloDefends(){
+        return getRandomNumber(3);
+    }
+    public static void battle(){
+       while(amigoLives > 0 && diabloLives > 0) {
+           if(amigoAttacks()==diabloDefends()) {
+               amigoLostLife();
+               System.out.println(diabloDefendPhrase);
+           }else {
+               diabloLostLife();
+               System.out.println(amigoAttackPhrase);
+           }
+       }
     }
 
 }
