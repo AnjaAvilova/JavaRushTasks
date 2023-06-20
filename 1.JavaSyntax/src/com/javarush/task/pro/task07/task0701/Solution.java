4f2b4ddfc5e8e7b3497d2febf4e4dc3f9df6573b
Anna A <markova29@yandex.ru>
2023-06-20 10:43:49 +0300

    add Solution Java Syntax task 0701

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0701/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task07.task0701;
+
+/* 
+Все возможные типы
+*/
+
+public class Solution {
+    int a;
+    long  b;
+    short c;
+    byte d;
+    boolean v;
+    double n;
+    char s;
+    float w;
+}
