d72161e3c9c2ddd33a074ad1de5bbf15d078c507
Anna A <markova29@yandex.ru>
2023-06-22 21:44:24 +0300

    add Solution Java Syntax task 0703

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0703/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task07.task0703;
+
+/* 
+Минимальное целое
+*/
+
+public class Solution {
+    byte aMin = Byte.MIN_VALUE;
+    int b = Integer.MIN_VALUE;
+    short c = Short.MIN_VALUE;
+    long d = Long.MIN_VALUE;
+
+}
