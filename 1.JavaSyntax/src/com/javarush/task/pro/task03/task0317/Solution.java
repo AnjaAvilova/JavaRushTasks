dd6c4e00e2472f92ed1bfe3e8035b2c6b2cb3816
Anna A <markova29@yandex.ru>
2023-08-14 12:21:00 +0300

    add Solution Java Syntax task 0317

--- 1.JavaSyntax/src/com/javarush/task/pro/task03/task0317/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0317/Solution.java
@@ -13,10 +13,16 @@ public class Solution {
         int number2 = scanner.nextInt();
         int number3 = scanner.nextInt();
         int number4 = scanner.nextInt();
-        if (int number > 0){
+        if (number1 > 0) {
             System.out.println(number1);
+        }
+        if (number2 > 0) {
             System.out.println(number2);
+        }
+        if (number3 > 0) {
             System.out.println(number3);
+        }
+        if (number4 > 0) {
             System.out.println(number4);
         }
     }
