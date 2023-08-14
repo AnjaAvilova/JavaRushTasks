3f0af602340eba6108896d7ae5e4797199a9c44b
Anna A <markova29@yandex.ru>
2023-08-14 12:20:32 +0300

    add Solution Java Syntax task 0211

--- 1.JavaSyntax/src/com/javarush/task/pro/task02/task0211/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0211/Solution.java
@@ -9,12 +9,9 @@ public class Solution {
     public static void main(String[] args) {
         String emptyString = "";
 
-        String Str1 = new String(emptyString);
-        String Str2 = new String("Gomu Gomu no Bazooka!");
-        String Str3 = new String (emptyString + 2 + 2 + "22");
 
-        System.out.println(emptyString);
-        System.out.println("Gomu Gomu no Bazooka!");
-        System.out.println((emptyString + 2 + 2 + "22"));
+        System.out.println(emptyString.length());
+        System.out.println("Gomu Gomu no Bazooka!".length());
+        System.out.println((emptyString + 2 + 2 + "22").length());
     }
 }
