52782aaffeaf021945d7ce75a8dbbe2a39ea17bb
Anna A <markova29@yandex.ru>
2023-10-19 17:17:40 +0300

    add Solution Java Syntax task 1104

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task11/task1104/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.pro.task11.task1104;
+
+/* 
+Конвертер строки
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String string = "12.84";
+        double a = Double.parseDouble(string);
+        int intOfa = (int)Math.round(a);
+        System.out.println(intOfa);
+    }
+}
