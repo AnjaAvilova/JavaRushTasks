4974f7d9072533be98412abf3ba36c5e26bac58e
Anna A <markova29@yandex.ru>
2024-02-04 20:07:44 +0300

    add Solution Java Syntax task 1214

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task12/task1214/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.pro.task12.task1214;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/* 
+Прощай, Паскаль
+*/
+
+public class Solution {
+    public static ArrayList<String> programmingLanguages = new ArrayList<>(Arrays.asList("C", "C++", "Python", "JavaScript", "Ruby", "Java", "Pascal"));
+
+    public static void main(String[] args) {
+        programmingLanguages.remove("Pascal");
+    }
+
+
+
+}
