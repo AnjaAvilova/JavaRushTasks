e92760225106249d9ff9acfae112cdde474107fc
Anna A <markova29@yandex.ru>
2024-02-19 20:47:00 +0300

    add Solution Java Syntax task 1422

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task14/task1422/Solution.java
@@ -0,0 +1,56 @@
+package com.javarush.task.pro.task14.task1422;
+
+/* 
+Сбор и сканирование ДНК
+*/
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        printDNA();
+    }
+
+    public static void printDNA() {
+        for (int i = 0; i < getDNA().size(); i++) {
+            StackTraceElement[] elements = getDNA().get(i);
+            StackTraceElement element = elements[1];
+            String fileName = element.getFileName();
+            String methodName = element.getMethodName();
+            int lineNumber = element.getLineNumber();
+
+            System.out.printf("File name: %s, Method name: %s, Line number: %d", fileName, methodName, lineNumber);
+            System.out.println();
+        }
+    }
+
+    private static List<StackTraceElement[]> getDNA() {
+        List<StackTraceElement[]> list = new ArrayList<>();
+
+        list.add(adenine());
+        list.add(thymine());
+        list.add(cytosine());
+        list.add(guanine());
+
+        return list;
+    }
+
+    private static StackTraceElement[] adenine() {
+        return Thread.currentThread().getStackTrace();
+    }
+
+    private static StackTraceElement[] thymine() {
+        return Thread.currentThread().getStackTrace();
+    }
+
+    private static StackTraceElement[] cytosine() {
+        return Thread.currentThread().getStackTrace();
+    }
+
+    private static StackTraceElement[] guanine() {
+        return Thread.currentThread().getStackTrace();
+    }
+}
