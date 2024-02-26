c34aa5b24611b29bee4488775d7ad4acbb4d61f1
Anna A <markova29@yandex.ru>
2024-02-26 20:55:06 +0300

    add Solution Java Syntax task 1317

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task13/task1317/MonthEnum.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task13.task1317;
+
+public enum MonthEnum {
+    JANUARY,
+    FEBRUARY,
+    MARCH,
+    APRIL,
+    MAY,
+    JUNE,
+    JULY,
+    AUGUST,
+    SEPTEMBER,
+    OCTOBER,
+    NOVEMBER,
+    DECEMBER
+}
