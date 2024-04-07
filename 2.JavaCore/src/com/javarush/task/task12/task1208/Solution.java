0e3dc63652f292b80b822c744c30cda4835e3ae1
Anna A <markova29@yandex.ru>
2024-04-07 20:12:51 +0300

    add Solution Java Syntax task1208

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1208/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static void print(){
+
+    }
+    public  static void print(int n) {
+
+    }
+    public static void print (String a) {
+
+    }
+    public static void print (int d, int s) {
+
+    }
+    public static void print (String d, int a) {
+
+    }
+}
