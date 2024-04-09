8630d9fc79aabac73e0df53ffd17d95991bdc06b
Anna A <markova29@yandex.ru>
2024-04-09 16:44:08 +0300

    add Solution Java Syntax task1217

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1217/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task12.task1217;
+
+/* 
+Лететь, бежать и плыть
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    interface CanFly {
+        static void canFly() {
+
+        }
+    }
+
+    interface CanRun {
+        static void canRun() {
+
+        }
+    }
+
+    interface CanSwim {
+        static void CanSwim() {
+
+        }
+    }
+//add interfaces here - добавь интерфейсы тут
+
+}
