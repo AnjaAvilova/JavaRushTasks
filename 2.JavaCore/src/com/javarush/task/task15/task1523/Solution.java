56b65cf3e4db020a5ef9b17be9fb525dcab7dfd5
Anna A <markova29@yandex.ru>
2024-12-04 00:14:42 +0300

    1523

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task15.task1523;
+
+/* 
+Перегрузка конструкторов
+*/
+
+public class Solution {
+    public Solution(){
+
+    }
+    private Solution(int number){
+
+    }
+    protected Solution(String f){
+
+    }
+    Solution(Object o){
+
+    }
+    public static void main(String[] args) {
+
+    }
+
+}
+
