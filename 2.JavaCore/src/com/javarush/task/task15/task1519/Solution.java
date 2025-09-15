f2c6f51e5d926ef8da7eac843bac0bffeba1b3f1
Anna A <markova29@yandex.ru>
2025-09-15 21:02:21 +0300

    задача 1519

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1519/Solution.java
@@ -0,0 +1,53 @@
+package com.javarush.task.task15.task1519;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/* 
+Разные методы для разных типов
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            String string = console.readLine();
+            if (string.equalsIgnoreCase("exit")) {
+                break;
+            }
+            try {
+                if (string.contains(".")) {
+                    double value = Double.parseDouble(string);
+                    print(value);
+                } else {
+                    int number = Integer.parseInt(string);
+                    if (number > 0 && number < 128) {
+                        print((short) number);
+                    } else {
+                        print(number);
+                    }
+                }
+            } catch (Exception e){
+                print(string);
+            }
+
+        }
+    }
+
+    public static void print(Double value) {
+        System.out.println("Это тип Double, значение " + value);
+    }
+
+    public static void print(String value) {
+        System.out.println("Это тип String, значение " + value);
+    }
+
+    public static void print(short value) {
+        System.out.println("Это тип short, значение " + value);
+    }
+
+    public static void print(Integer value) {
+        System.out.println("Это тип Integer, значение " + value);
+    }
+}
